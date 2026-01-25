502957c663d687f30e8be98823e12617559bef09
Justin Aguilar <justin.mikael16@gmail.com>
2026-01-25 13:43:26 +0800

    add CampusInfoUiState for list state

--- app/src/main/java/com/example/smartcampusapp/feature/campusinfo/state/CampusInfoUiState.kt
+++ app/src/main/java/com/example/smartcampusapp/feature/campusinfo/state/CampusInfoUiState.kt
@@ -1,3 +1,16 @@
 package com.example.smartcampusapp.feature.campusinfo.state
 
-// Campus info UI state
+import com.example.smartcampusapp.model.CampusInfoItem
+
+/**
+ * UI state for the Campus Information screen.
+ * 
+ * @param isLoading Indicates if data is currently being loaded
+ * @param departments List of campus departments to display
+ * @param selectedDepartment Currently selected department (if any)
+ */
+data class CampusInfoUiState(
+    val isLoading: Boolean = false,
+    val departments: List<CampusInfoItem> = emptyList(),
+    val selectedDepartment: CampusInfoItem? = null
+)
