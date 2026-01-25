52e9d1ae32d479c889de277e4d3f5b9808b7a6ab
Justin Aguilar <justin.mikael16@gmail.com>
2026-01-25 13:37:42 +0800

    create CampusInfoItem model for department data

--- app/src/main/java/com/example/smartcampusapp/model/CampusInfoItem.kt
+++ app/src/main/java/com/example/smartcampusapp/model/CampusInfoItem.kt
@@ -1,3 +1,18 @@
 package com.example.smartcampusapp.model
 
-// Simple UI model for campus info
+/**
+ * Data model representing a campus department or information item.
+ * 
+ * @param id Unique identifier for the department
+ * @param name Department name
+ * @param contact Contact phone number
+ * @param email Department email address
+ * @param location Physical location of the department
+ */
+data class CampusInfoItem(
+    val id: Int,
+    val name: String,
+    val contact: String,
+    val email: String,
+    val location: String
+)
